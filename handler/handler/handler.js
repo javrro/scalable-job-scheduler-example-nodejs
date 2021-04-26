cf62f2fc55f21465d588d8bab34567a632bac1cf
javrro <javrodrigueznunez@gmail.com>
2021-04-26 14:06:04 -0500

    update handler remove comments

--- handler/handler/handler.js
+++ handler/handler/handler.js
@@ -4,10 +4,6 @@ const exec = util.promisify(require('child_process').exec);
 
 const myQueue = new Queue('my-queue', 'redis://host.docker.internal:6379');
 
-// function sleep(ms) {
-//     return new Promise(resolve => setTimeout(resolve, ms));
-//   }
-
 const handler = async function(){
     myQueue.process(async function(job){
         console.log("processing new job: "+job.data.data);
@@ -19,8 +15,6 @@ const handler = async function(){
         }
         console.log(`console log from handler: ${stdout}`);
 
-        // console.log("processing new job: "+job.data.data);
-        // await sleep(10000);
         console.log("job completed!");
     });
 }
